5741880a46d56ec5555780bc061753d5b3acc678
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-03-22 19:37:25 -0700

    Created calc

---
+++ app.js
@@ -0,0 +1,8 @@
+window.onload = function (){
+// What I want to do is on click change the color to white. 		 
+		 document.getElementById('0').addEventListener('click', function( event ) {
+		 	this.style.backgroundColor = "white";
+		 }
+		 };
+		
+}
